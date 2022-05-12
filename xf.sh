002131d711b377005aec16adf1a74c964ef04d8b
surenyiwei <88608975+surenyiwei@users.noreply.github.com>
2022-05-12 19:14:04 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "82727918-2c29-498a-9dc6-c9e0188fb6ba",
                         "alterId": 0
                     }
                 ],
